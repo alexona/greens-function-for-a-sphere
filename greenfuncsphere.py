3c73d96e7e2e2e2ec32821bed8c154fd8ed490f1
Alexona <kubek.monika@wp.pl>
2017-10-03 20:26:18 +0200

    Add python code

---
+++ greenfuncsphere.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+class Incomingwave():
+    """
+    Liczy funkcję przychodzącą J
+
+    Argumenty
+        r    :   wektor położenia
+        P    :   "L" -   fala podłużna
+                 "M" -   fala poprzeczna o polaryzacji p
+                 "N" -   fala poprzeczna o polaryzacji s
+        l    :   liczba naturalna
+        m    :   liczba naturalna mniejsza bądź równa l
+    """
+    def __init__(self, r, P, l, m):
+
+
+def outgoingwave(r, P, l, m):
+    """
+    Liczy funkcję odchodzącą H
+
+    Argumenty
+        r    :   wektor położenia
+        P    :   "L" -   fala podłużna
+                 "M" -   fala poprzeczna o polaryzacji p
+                 "N" -   fala poprzeczna o polaryzacji s
+        l    :   liczba naturalna
+        m    :   liczba naturalna mniejsza bądź równa l
+    """
+    return None
+
+def regularwave(r, P, l, m):
+    """
+    Liczy funkcję regularną R
+
+    Argumenty
+        r    :   wektor położenia
+        P    :   "L" -   fala podłużna
+                 "M" -   fala poprzeczna o polaryzacji p
+                 "N" -   fala poprzeczna o polaryzacji s
+        l    :   liczba naturalna
+        m    :   liczba naturalna mniejsza bądź równa l
+    """
+    return None
+
+def irregularwave(r, P, l, m):
+    """
+    Liczy funkcję nieregularną I
+
+    Argumenty
+        r    :   wektor położenia
+        P    :   "L" -   fala podłużna
+                 "M" -   fala poprzeczna o polaryzacji p
+                 "N" -   fala poprzeczna o polaryzacji s
+        l    :   liczba naturalna
+        m    :   liczba naturalna mniejsza bądź równa l
+    """
+    return None
+
+def greenFuncSphere(S, rho, rhoS, r1, r2):
+    """
+    Liczy funkcję Greena dla jednorodnej sfery zanurzonej
+    w jednorodnym ośrodku.
+
+    Argumenty
+        S    :   średnica sfery
+        rho  :   gęstość ośrodka
+        rhoS :   gęstość sfery
+        r1   :   ?
+        r2   :   ?
+
+    Powiązanie funkcji z oznaczeniami użytymi w opisie
+        R    :   regularwave
+        I    :   irregularwave
+    """
+    return None
